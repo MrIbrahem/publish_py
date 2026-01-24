4e438e9a233a0f66f79757e09ff7a32660cdd6ff
ibrahem Qasim <ibrahemqasim@wikiprojectmed.org>
2026-01-24 13:41:40 +0300

    Delete php_src directory

--- php_src/cxtoken.php
+++
@@ -1,2 +0,0 @@
-<?PHP
-include_once __DIR__ . '/endpoints/cxtoken.php';
--- php_src/text_change.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WpRefs\FixPage;
-
-function DoChangesToText1($sourcetitle, $title, $text, $lang, $mdwiki_revid)
-{
-    // place holder for future changes
-    return $text;
-}
