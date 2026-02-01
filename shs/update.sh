03e3da63bd50a54db3b53b4d789b7a3860caad6a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-01 05:49:25 +0300

    Add deployment and update scripts, update example.env
    Added shs/deploy_repo.sh and shs/update.sh for automating repository deployment and updates. Updated src/example.env to remove unused and deprecated environment variables, and cleaned up configuration for clarity.

---
+++ shs/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# toolforge-jobs run update --image python3.11 --command "~/shs/update.sh" --wait
+export SUB_DIR_COPY="src"
+export COPY_TO_TARGET="requirements.txt"
+export CLEAN_INSTALL=1
+export USER_NAME="MrIbrahem"
+BRANCH="${1:-main}"
+REPO=publish_py
+TARGET_PATH=~/www/python/src
+$HOME/shs/deploy_repo.sh "$REPO" "$TARGET_PATH" "$BRANCH"
+
+if source "$HOME/www/python/venv/bin/activate"; then
+    pip install -r $HOME/www/python/src/requirements.txt
+    # exit 1
+else
+    echo "Failed to activate virtual environment" >&2
+fi
