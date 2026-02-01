ab6a5b28b089e48ea7d00422d30a92ee754416f6
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-01 04:09:07 +0300

    Improve .env file path resolution logic
    Refactors the logic for determining the .env file path to prioritize the local directory, then fallback to $HOME/.env, and finally $HOME/confs/.env if previous paths do not exist. This makes environment configuration more flexible and robust.

--- src/env_config.py
+++ src/env_config.py
@@ -7,11 +7,13 @@ import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
-
+_env_file_path = str(Path(__file__).parent / ".env")
 _HOME = os.getenv("HOME")
-_env_file_path = f"{_HOME}/confs/.env"
 
-if _HOME is None or _HOME == "":
-    _env_file_path = str(Path(__file__).parent / ".env")
+if _HOME and not _env_file_path.exists():
+    _env_file_path = Path(f"{_HOME}/.env")
+
+    if not _env_file_path.exists():
+        _env_file_path = Path(f"{_HOME}/confs/.env")
 
 load_dotenv(_env_file_path)
