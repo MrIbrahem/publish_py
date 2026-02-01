bc709b7362875844187b209b213653c92751b552
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-01 06:49:29 +0300

    1

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
-
-from env_config import _env_file_path  # noqa: F401 # Triggers environment configuration
+try:
+    from env_config import _env_file_path  # noqa: F401 # Triggers environment configuration
+except ImportError:
+    from .env_config import _env_file_path  # noqa: F401 # Triggers environment configuration
