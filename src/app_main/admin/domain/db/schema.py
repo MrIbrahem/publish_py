49a85ffd119a4bac0310af1e9cc09900bf51c56f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-16 09:15:59 +0300

    .

--- src/app_main/admin/domain/db/schema.py
+++ src/app_main/admin/domain/db/schema.py
@@ -15,8 +15,7 @@ class TablesCreatesSql:
 
 coordinator= """
 
-CREATE TABLE
-  IF NOT EXISTS coordinator (
+CREATE TABLE IF NOT EXISTS coordinator (
     id int unsigned NOT NULL AUTO_INCREMENT,
     user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     active int NOT NULL DEFAULT '1',
@@ -27,8 +26,7 @@ CREATE TABLE
 
 full_translators= """
 
-CREATE TABLE
-  IF NOT EXISTS full_translators (
+CREATE TABLE IF NOT EXISTS full_translators (
     id int unsigned NOT NULL AUTO_INCREMENT,
     user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     active int NOT NULL DEFAULT '1',
@@ -38,8 +36,7 @@ CREATE TABLE
 """
 
 language_settings= """
-CREATE TABLE
-  IF NOT EXISTS language_settings (
+CREATE TABLE IF NOT EXISTS language_settings (
     id int NOT NULL AUTO_INCREMENT,
     lang_code varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     move_dots tinyint DEFAULT '0',
@@ -53,8 +50,7 @@ CREATE TABLE
 
 settings= """
 
-CREATE TABLE
-  IF NOT EXISTS settings (
+CREATE TABLE IF NOT EXISTS settings (
     id int NOT NULL AUTO_INCREMENT,
     title varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
     displayed varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -68,8 +64,7 @@ CREATE TABLE
 
 users_no_inprocess= """
 
-CREATE TABLE
-  IF NOT EXISTS users_no_inprocess (
+CREATE TABLE IF NOT EXISTS users_no_inprocess (
     id int unsigned NOT NULL AUTO_INCREMENT,
     user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     active int NOT NULL DEFAULT '1',
--- src/app_main/public/domain/db/schema.py
+++ src/app_main/public/domain/db/schema.py
@@ -22,8 +22,7 @@ class PublicSqlSchema:
 
 
 assessments = """
-CREATE TABLE
-  IF NOT EXISTS assessments (
+CREATE TABLE IF NOT EXISTS assessments (
     id int unsigned NOT NULL AUTO_INCREMENT,
     title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     importance varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -34,8 +33,7 @@ CREATE TABLE
 
 enwiki_pageviews = """
 
-CREATE TABLE
-  IF NOT EXISTS enwiki_pageviews (
+CREATE TABLE IF NOT EXISTS enwiki_pageviews (
     id int unsigned NOT NULL AUTO_INCREMENT,
     title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     en_views int DEFAULT '0',
@@ -46,8 +44,7 @@ CREATE TABLE
 
 in_process = """
 
-CREATE TABLE
-  IF NOT EXISTS in_process (
+CREATE TABLE IF NOT EXISTS in_process (
     id int unsigned NOT NULL AUTO_INCREMENT,
     title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     user varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -63,8 +60,7 @@ CREATE TABLE
 
 langs = """
 
-CREATE TABLE
-  IF NOT EXISTS langs (
+CREATE TABLE IF NOT EXISTS langs (
     lang_id int NOT NULL AUTO_INCREMENT,
     code varchar(20) CHARACTER
     SET
@@ -81,8 +77,7 @@ CREATE TABLE
 
 mdwiki_revids = """
 
-CREATE TABLE
-  IF NOT EXISTS mdwiki_revids (
+CREATE TABLE IF NOT EXISTS mdwiki_revids (
     title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     revid int NOT NULL,
     PRIMARY KEY (title)
@@ -91,8 +86,7 @@ CREATE TABLE
 
 pages_users_to_main = """
 
-CREATE TABLE
-  IF NOT EXISTS pages_users_to_main (
+CREATE TABLE IF NOT EXISTS pages_users_to_main (
     id int unsigned NOT NULL,
     new_target varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
     new_user varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
@@ -104,8 +98,7 @@ CREATE TABLE
 
 projects = """
 
-CREATE TABLE
-  IF NOT EXISTS projects (
+CREATE TABLE IF NOT EXISTS projects (
     g_id int unsigned NOT NULL AUTO_INCREMENT,
     g_title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     PRIMARY KEY (g_id),
@@ -114,37 +107,30 @@ CREATE TABLE
 """
 
 refs_counts = """
-
-
-CREATE TABLE
-  IF NOT EXISTS refs_counts (
+CREATE TABLE IF NOT EXISTS refs_counts (
     r_id int unsigned NOT NULL AUTO_INCREMENT,
     r_title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     r_lead_refs int DEFAULT NULL,
     r_all_refs int DEFAULT NULL,
     PRIMARY KEY (r_id),
-    UNIQUE KEY r_title (r_title),
-    KEY idx_refs_counts_r_title (r_title)
+    UNIQUE KEY r_title (r_title)
   )
 """
 
 translate_type = """
 
-CREATE TABLE
-  IF NOT EXISTS translate_type (
+CREATE TABLE IF NOT EXISTS translate_type (
     tt_id int unsigned NOT NULL AUTO_INCREMENT,
     tt_title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     tt_lead int NOT NULL DEFAULT '1',
     tt_full int NOT NULL DEFAULT '0',
     PRIMARY KEY (tt_id),
-    UNIQUE KEY tt_title (tt_title),
-    KEY idx_tt_title (tt_title)
+    UNIQUE KEY tt_title (tt_title)
   )
 """
 
 users = """
-CREATE TABLE
-  IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     user_id int NOT NULL AUTO_INCREMENT,
     username varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     email varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
@@ -157,8 +143,7 @@ CREATE TABLE
 
 views_new = """
 
-CREATE TABLE
-  IF NOT EXISTS views_new (
+CREATE TABLE IF NOT EXISTS views_new (
     id int unsigned NOT NULL AUTO_INCREMENT,
     target varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     lang varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -172,15 +157,13 @@ CREATE TABLE
 
 words = """
 
-CREATE TABLE
-  IF NOT EXISTS words (
+CREATE TABLE IF NOT EXISTS words (
     w_id int unsigned NOT NULL AUTO_INCREMENT,
     w_title varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
     w_lead_words int DEFAULT NULL,
     w_all_words int DEFAULT NULL,
     PRIMARY KEY (w_id),
-    UNIQUE KEY w_title (w_title),
-    KEY idx_words_w_title (w_title)
+    UNIQUE KEY w_title (w_title)
   )
 """
 
