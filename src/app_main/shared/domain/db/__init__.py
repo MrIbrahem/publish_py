c159d2b03cc139a1f0e6265d648e03a8563dfcea
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-16 03:58:13 +0300

    Update __init__.py

--- src/app_main/shared/domain/db/__init__.py
+++ src/app_main/shared/domain/db/__init__.py
@@ -3,6 +3,7 @@ Shared dbs, used in both admin and public blueprints
 """
 
 from .db_categories import CategoriesDB, clear_categories_cache, get_campaign_category
+from .db_pages import PagesDB
 from .db_publish_reports import ReportsDB
 from .db_qids import QidsDB
 from .db_user_tokens import UserTokenDB
@@ -11,6 +12,7 @@ from .schema import sql_tables
 __all__ = [
     "UserTokenDB",
     "CategoriesDB",
+    "PagesDB",
     "ReportsDB",
     "QidsDB",
     "clear_categories_cache",
