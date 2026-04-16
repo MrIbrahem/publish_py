8e91fd4e594149b4171eb49a087f6ceef41b39d4
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-16 08:10:32 +0300

    .

--- src/app_main/admin/domain/db_ensure_tables.py
+++ src/app_main/admin/domain/db_ensure_tables.py
@@ -8,10 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 def ensure_db_tables(db_data: DbConfig) -> None:
-    db = Database(db_data)
-
-    db.execute_query_safe(admin_sql_tables.coordinator)
-    db.execute_query_safe(admin_sql_tables.full_translators)
-    db.execute_query_safe(admin_sql_tables.language_settings)
-    db.execute_query_safe(admin_sql_tables.settings)
-    db.execute_query_safe(admin_sql_tables.users_no_inprocess)
+    with Database(db_data) as db:
+        db.execute_query_safe(admin_sql_tables.coordinator)
+        db.execute_query_safe(admin_sql_tables.full_translators)
+        db.execute_query_safe(admin_sql_tables.language_settings)
+        db.execute_query_safe(admin_sql_tables.settings)
+        db.execute_query_safe(admin_sql_tables.users_no_inprocess)
