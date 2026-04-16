a941b1ddbceae10aec75d02ed5d6fc81c649642f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-16 07:25:29 +0300

    Update schema.py

--- src/app_main/admin/domain/db/schema.py
+++ src/app_main/admin/domain/db/schema.py
@@ -1,51 +1,4 @@
 """
-CREATE TABLE
-  IF NOT EXISTS coordinator (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-    active int NOT NULL DEFAULT '1',
-    PRIMARY KEY (id),
-    UNIQUE KEY user (user)
-  )
-CREATE TABLE
-  IF NOT EXISTS full_translators (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-    active int NOT NULL DEFAULT '1',
-    PRIMARY KEY (id),
-    UNIQUE KEY user (user)
-  )
-CREATE TABLE
-  IF NOT EXISTS language_settings (
-    id int NOT NULL AUTO_INCREMENT,
-    lang_code varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    move_dots tinyint DEFAULT '0',
-    expend tinyint DEFAULT '0',
-    add_en_lang tinyint DEFAULT '0',
-    add_en_lng tinyint DEFAULT '0',
-    PRIMARY KEY (id),
-    UNIQUE KEY lang_code (lang_code)
-  )
-CREATE TABLE
-  IF NOT EXISTS settings (
-    id int NOT NULL AUTO_INCREMENT,
-    title varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-    displayed varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-    Type varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'check',
-    value int NOT NULL DEFAULT '0',
-    ignored int NOT NULL DEFAULT '0',
-    PRIMARY KEY (id),
-    UNIQUE KEY title (title),
-    KEY idx_title (title)
-  )
-CREATE TABLE
-  IF NOT EXISTS users_no_inprocess (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-    active int NOT NULL DEFAULT '1',
-    PRIMARY KEY (id),
-    UNIQUE KEY user (user)
-  )
 """
 
 from dataclasses import dataclass
@@ -60,6 +13,72 @@ class TablesCreatesSql:
     users_no_inprocess: str
 
 
+coordinator= """
+
+CREATE TABLE
+  IF NOT EXISTS coordinator (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+    active int NOT NULL DEFAULT '1',
+    PRIMARY KEY (id),
+    UNIQUE KEY user (user)
+  )
+"""
+
+full_translators= """
+
+CREATE TABLE
+  IF NOT EXISTS full_translators (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+    active int NOT NULL DEFAULT '1',
+    PRIMARY KEY (id),
+    UNIQUE KEY user (user)
+  )
+"""
+
+language_settings= """
+CREATE TABLE
+  IF NOT EXISTS language_settings (
+    id int NOT NULL AUTO_INCREMENT,
+    lang_code varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    move_dots tinyint DEFAULT '0',
+    expend tinyint DEFAULT '0',
+    add_en_lang tinyint DEFAULT '0',
+    add_en_lng tinyint DEFAULT '0',
+    PRIMARY KEY (id),
+    UNIQUE KEY lang_code (lang_code)
+  )
+"""
+
+settings= """
+
+CREATE TABLE
+  IF NOT EXISTS settings (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+    displayed varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+    Type varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'check',
+    value int NOT NULL DEFAULT '0',
+    ignored int NOT NULL DEFAULT '0',
+    PRIMARY KEY (id),
+    UNIQUE KEY title (title),
+    KEY idx_title (title)
+  )
+"""
+
+users_no_inprocess= """
+
+CREATE TABLE
+  IF NOT EXISTS users_no_inprocess (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    user varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+    active int NOT NULL DEFAULT '1',
+    PRIMARY KEY (id),
+    UNIQUE KEY user (user)
+  )
+
+"""
 # sql_tables
 sql_tables = TablesCreatesSql(
     coordinator=coordinator,
