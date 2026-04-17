76bd7a2ed7cd13dd05710161f70cbaa8cfc477ef
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-18 00:52:20 +0300

    .

--- src/app_main/admin/domain/models/__init__.py
+++ src/app_main/admin/domain/models/__init__.py
@@ -6,14 +6,12 @@ from .coordinator import CoordinatorRecord
 from .full_translator import FullTranslatorRecord
 from .language_setting import LanguageSettingRecord
 from .setting import SettingRecord
-from .setting import SettingRecord
 from .users_no_inprocess import UsersNoInprocessRecord
 
 __all__ = [
     "CoordinatorRecord",
     "FullTranslatorRecord",
     "LanguageSettingRecord",
-    "SettingRecord",
     "SettingRecord",
     "UsersNoInprocessRecord",
 ]
