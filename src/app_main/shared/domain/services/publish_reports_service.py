5acc19484a053762ef2953852e8ebd1ed3e89eb9
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-18 03:57:56 +0300

    Update publish_reports_service.py

--- src/app_main/shared/domain/services/publish_reports_service.py
+++ src/app_main/shared/domain/services/publish_reports_service.py
@@ -9,7 +9,7 @@ from typing import List
 
 from ....config import settings
 from ..db.db_publish_reports import ReportsDB
-from ..models import CategoryRecord
+from ..models import ReportRecord
 from ..db_service import has_db_config
 
 logger = logging.getLogger(__name__)
@@ -34,45 +34,29 @@ def get_publish_reports_db() -> ReportsDB:
 
 
 def add_report(
-    category: str,
-    display: str | None = "",
-    campaign: str | None = "",
-    category2: str | None = "",
-    depth: int = 0,
-    is_default: int = 0,
-) -> CategoryRecord:
-    """Add a category."""
-
-    # fallback display to campaign name if display name is not provided
-    display = display or campaign
+    title: str,
+    user: str,
+    lang: str,
+    sourcetitle: str,
+    result: str,
+    data: str,
+) -> ReportRecord:
+    """Add a report."""
 
     store = get_publish_reports_db()
-    record = store.add(category, display, campaign, category2, depth)
-
-    if is_default:
-        # set this category as default by unsetting default flag on all other publish_reports
-        store.set_default(record.id)
-
-    return record
-
-
-def update_report(report_id: int, title: str, main_file: str) -> CategoryRecord:
-    """Update category."""
-
-    store = get_publish_reports_db()
-    record = store.update(report_id, title, main_file)
+    record = store.add(title, user, lang, sourcetitle, result, data)
 
     return record
 
 
 def delete_report(report_id: int) -> None:
-    """Delete a category."""
+    """Delete a report."""
 
     store = get_publish_reports_db()
     store.delete(report_id)
 
 
-def list_publish_reports() -> List[CategoryRecord]:
+def list_publish_reports() -> List[ReportRecord]:
     """Return all publish_reports."""
 
     store = get_publish_reports_db()
@@ -83,7 +67,6 @@ def list_publish_reports() -> List[CategoryRecord]:
 
 __all__ = [
     "add_report",
-    "update_report",
     "delete_report",
     "list_publish_reports",
 ]
