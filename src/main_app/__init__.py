fa33c0f0ea34ddb7002b50a4220ae7ba5cc8c135
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-23 02:09:21 +0300

    Update __init__.py

--- src/main_app/__init__.py
+++ src/main_app/__init__.py
@@ -14,12 +14,10 @@ from .admin import register_bp_admin_blueprints
 from .config import ensure_directories, settings
 from .db import init_db
 from .db.exceptions import DatabaseInitError
-from .extensions import (
-    csrf_init_app,
-)
 from .extensions import db as _db
 from .extensions import (
     migrate,
+    csrf_init_app,
 )
 from .public import register_blueprints
 from .public.utils import context_data
