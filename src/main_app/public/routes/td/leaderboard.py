b5de2af9cf95a063d285e3063ba47ffd549475ca
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-23 07:27:37 +0300

    Update leaderboard.py

--- src/main_app/public/routes/td/leaderboard.py
+++ src/main_app/public/routes/td/leaderboard.py
@@ -33,154 +33,154 @@ class LeaderBoardRoutes:
         self._setup_routes()
 
     def _setup_routes(self) -> None:
+        self.bp.route("/users/<string:username>", methods=["GET"])(self.users)
+        self.bp.route("/langs/<string:lang_code>", methods=["GET"])(self.langs)
+        self.bp.route("/js", methods=["GET"])(self.index_js)
+        self.bp.route("/", methods=["GET"])(self.index)
 
-        @self.bp.get("/js")
-        def index_js() -> str:
-            year = request.args.get("year", type=int)
-            # month = request.args.get("month", type=int)
-            camp = request.args.get("camp", type=str)
-            campaign_to_cats = get_camp_to_cats()
+    def index_js(self) -> str:
+        year = request.args.get("year", type=int)
+        # month = request.args.get("month", type=int)
+        camp = request.args.get("camp", type=str)
+        campaign_to_cats = get_camp_to_cats()
 
-            form_data = self.load_form_data(list(campaign_to_cats.keys()), year)
+        form_data = self.load_form_data(list(campaign_to_cats.keys()), year)
 
-            cat = campaign_to_cats.get(camp) if camp and camp != "all" else None
-            chart_data = self.load_chart_data(cat, year, camp)
+        cat = campaign_to_cats.get(camp) if camp and camp != "all" else None
+        chart_data = self.load_chart_data(cat, year, camp)
 
-            numbers_summary = {
-                "users": 0,
-                "articles": 0,
-                "words": 0,
-                "languages": 0,
-                "pageviews": 0,
-            }
-            form_selected_data = request.args
+        numbers_summary = {
+            "users": 0,
+            "articles": 0,
+            "words": 0,
+            "languages": 0,
+            "pageviews": 0,
+        }
+        form_selected_data = request.args
 
-            return render_template(
-                "td/leaderboard/index-js.html",
-                # data to use in form
-                form_data=form_data,
-                selected_data=form_selected_data,
-                chart_data=chart_data,
-                numbers_summary=numbers_summary,
-            )
+        return render_template(
+            "td/leaderboard/index-js.html",
+            # data to use in form
+            form_data=form_data,
+            selected_data=form_selected_data,
+            chart_data=chart_data,
+            numbers_summary=numbers_summary,
+        )
 
-        @self.bp.get("/")
-        def index() -> str:
-            year = request.args.get("year", type=int)
-            # month = request.args.get("month", type=int)
-            camp = request.args.get("camp", type=str)
-            campaign_to_cats = get_camp_to_cats()
+    def index(self) -> str:
+        year = request.args.get("year", type=int)
+        # month = request.args.get("month", type=int)
+        camp = request.args.get("camp", type=str)
+        campaign_to_cats = get_camp_to_cats()
 
-            form_data = self.load_form_data(list(campaign_to_cats.keys()), year)
+        form_data = self.load_form_data(list(campaign_to_cats.keys()), year)
 
-            cat = campaign_to_cats.get(camp) if camp and camp != "all" else None
-            chart_data = self.load_chart_data(cat, year, camp)
+        cat = campaign_to_cats.get(camp) if camp and camp != "all" else None
+        chart_data = self.load_chart_data(cat, year, camp)
 
-            form_selected_data = request.args
+        form_selected_data = request.args
 
-            langs_data = get_top_langs(request.args)
-            users_data = get_top_users(request.args)
+        langs_data = get_top_langs(request.args)
+        users_data = get_top_users(request.args)
 
-            result = {
-                "langs": langs_data.get("results") or [],
-                "users": users_data.get("results") or [],
-                "users_top_langs": {},
-            }
+        result = {
+            "langs": langs_data.get("results") or [],
+            "users": users_data.get("results") or [],
+            "users_top_langs": {},
+        }
 
-            if users_data.get("results"):
-                # {row["user"]: {"lang": row["lang"], "count": row["count"]} for row in result_list}
-                users_top_langs: list[dict[Any, Any]] = top_lang_of_users()
-                result["users_top_langs"] = {row["user"]: row for row in users_top_langs}
+        if users_data.get("results"):
+            # {row["user"]: {"lang": row["lang"], "count": row["count"]} for row in result_list}
+            users_top_langs: list[dict[Any, Any]] = top_lang_of_users()
+            result["users_top_langs"] = {row["user"]: row for row in users_top_langs}
 
-            users_total = users_data.get("count") or 0
-            langs_total = langs_data.get("count") or 0
+        users_total = users_data.get("count") or 0
+        langs_total = langs_data.get("count") or 0
 
-            numbers_summary = self.load_summary_data(result["users"], users_total, langs_total)
+        numbers_summary = self.load_summary_data(result["users"], users_total, langs_total)
 
-            return render_template(
-                "td/leaderboard/index.html",
-                # data to use in form
-                form_data=form_data,
-                selected_data=form_selected_data,
-                chart_data=chart_data,
-                numbers_summary=numbers_summary,
-                result=result,  # main data
-            )
+        return render_template(
+            "td/leaderboard/index.html",
+            # data to use in form
+            form_data=form_data,
+            selected_data=form_selected_data,
+            chart_data=chart_data,
+            numbers_summary=numbers_summary,
+            result=result,  # main data
+        )
 
-        @self.bp.get("/langs/<string:lang_code>")
-        def langs(lang_code: str) -> str:
-            selected_year = request.args.get("year", type=int)
-            lang_years: list[int] = get_pages_years(lang=lang_code)
+    def langs(self, lang_code: str) -> str:
+        selected_year = request.args.get("year", type=int)
+        lang_years: list[int] = get_pages_years(lang=lang_code)
 
-            lang_pages = get_pages(
-                year=selected_year,
-                lang=lang_code,
-            )
+        lang_pages = get_pages(
+            year=selected_year,
+            lang=lang_code,
+        )
 
-            words_total = sum(int(page["word"]) for page in lang_pages if page.get("word"))
-            pageviews_total = sum(int(page["views"]) for page in lang_pages if page.get("views"))
+        words_total = sum(int(page["word"]) for page in lang_pages if page.get("word"))
+        pageviews_total = sum(int(page["views"]) for page in lang_pages if page.get("views"))
 
-            chart_data = get_chart_data_formatted(
-                lang=lang_code,
-                year=selected_year,
-            )
-            return render_template(
-                "td/leaderboard/langs.html",
-                lang_code=lang_code,
-                # data to use in form
-                form_data={
-                    "years": lang_years,
-                },
-                selected_data={
-                    "year": selected_year,
-                },
-                words_total=words_total,
-                pageviews_total=pageviews_total,
-                chart_data=chart_data,
-                pages=lang_pages,  # main data
-            )
+        chart_data = get_chart_data_formatted(
+            lang=lang_code,
+            year=selected_year,
+        )
+        return render_template(
+            "td/leaderboard/langs.html",
+            lang_code=lang_code,
+            # data to use in form
+            form_data={
+                "years": lang_years,
+            },
+            selected_data={
+                "year": selected_year,
+            },
+            words_total=words_total,
+            pageviews_total=pageviews_total,
+            chart_data=chart_data,
+            pages=lang_pages,  # main data
+        )
 
-        @self.bp.get("/users/<string:username>")
-        def users(username: str) -> str:
-            selected_year = request.args.get("year", type=int)
-            selected_lang = request.args.get("lang", type=str)
+    def users(self, username: str) -> str:
+        selected_year = request.args.get("year", type=int)
+        selected_lang = request.args.get("lang", type=str)
 
-            user_years: list[int] = get_pages_years(user=username)
-            user_langs = top_lang_of_user(username)
+        user_years: list[int] = get_pages_years(user=username)
+        user_langs = top_lang_of_user(username)
 
-            user_pages = get_pages(
-                user=username,
-                year=selected_year,
-                lang=selected_lang,
-            )
-            words_total = sum(page["word"] for page in user_pages if page.get("word"))
-            pageviews_total = sum(page["views"] for page in user_pages if page.get("views"))
+        user_pages = get_pages(
+            user=username,
+            year=selected_year,
+            lang=selected_lang,
+        )
+        words_total = sum(page["word"] for page in user_pages if page.get("word"))
+        pageviews_total = sum(page["views"] for page in user_pages if page.get("views"))
 
-            chart_data = get_chart_data_formatted(
-                user=username,
-                year=selected_year,
-                lang=selected_lang,
-            )
+        chart_data = get_chart_data_formatted(
+            user=username,
+            year=selected_year,
+            lang=selected_lang,
+        )
 
-            form_data = {
-                "years": user_years,
-                "langs": user_langs,
-            }
+        form_data = {
+            "years": user_years,
+            "langs": user_langs,
+        }
 
-            return render_template(
-                "td/leaderboard/users.html",
-                username=username,
-                # data to use in form
-                form_data=form_data,
-                selected_data={
-                    "year": selected_year,
-                    "lang": selected_lang,
-                },
-                words_total=words_total,
-                pageviews_total=pageviews_total,
-                chart_data=chart_data,
-                pages=user_pages,  # main data
-            )
+        return render_template(
+            "td/leaderboard/users.html",
+            username=username,
+            # data to use in form
+            form_data=form_data,
+            selected_data={
+                "year": selected_year,
+                "lang": selected_lang,
+            },
+            words_total=words_total,
+            pageviews_total=pageviews_total,
+            chart_data=chart_data,
+            pages=user_pages,  # main data
+        )
 
     def load_chart_data(self, cat, year, camp):
         user_group = request.args.get("user_group", type=str)
@@ -209,7 +209,10 @@ class LeaderBoardRoutes:
         return form_data
 
     def load_summary_data(
-        self, result_users: list[dict[str, Any]], users_total: int, langs_total: int
+        self,
+        result_users: list[dict[str, Any]],
+        users_total: int,
+        langs_total: int,
     ) -> dict[str, int]:
         summary_data = {
             "users": users_total,
