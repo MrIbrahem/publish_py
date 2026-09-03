eceb454608a14ee26056d29c6fe7986108963e44
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-03 07:32:17 +0300

    .

---
+++ tests/unit/public/routes/html_to_segments/lib/lineardoc/test_contextualizer.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+"""
+Unit tests for src/main_app/public/routes/html_to_segments/lib/lineardoc/contextualizer.py module.
+
+Classes to test: Contextualizer
+
+TODO: write tests
+"""
+
+
+from src.main_app.public.routes.html_to_segments.lib.lineardoc.contextualizer import (
+    Contextualizer,
+)
---
+++ tests/unit/public/routes/html_to_segments/lib/lineardoc/test_doc.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+"""
+Unit tests for src/main_app/public/routes/html_to_segments/lib/lineardoc/doc.py module.
+
+Classes to test: Doc
+
+TODO: write tests
+"""
+
+
+from src.main_app.public.routes.html_to_segments.lib.lineardoc.doc import (
+    Doc,
+)
