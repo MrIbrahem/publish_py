fcb5ba2609d6e8ef1ccc911ba66b0cde63487552
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-05 01:36:04 +0300

    .

--- tests/unit/public/routes/html_to_segments/lib/lineardoc/test_normalizer.py
+++ tests/unit/public/routes/html_to_segments/lib/lineardoc/test_normalizer.py
@@ -3,13 +3,9 @@
 Unit tests for src/main_app/public/routes/html_to_segments/lib/lineardoc/normalizer.py module.
 
 Classes to test: Normalizer
-Functions to test: esc
 
 TODO: write tests
 """
 
 
-from src.main_app.public.routes.html_to_segments.lib.lineardoc.normalizer import (
-    Normalizer,
-    esc,
-)
+from src.main_app.public.routes.html_to_segments.lib.lineardoc.normalizer import Normalizer
