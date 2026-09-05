60da291d09b6b6499643fd2cce06641e00d7537a
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-06 00:54:11 +0300

    Merge branch 'main' into z

--- src/main_app/services/new_html_services/__init__.py
+++ src/main_app/services/new_html_services/__init__.py
@@ -1,11 +1,12 @@
 from .html_utils import remove_data_parsoid
-from .process import WikitextFixerService, process_page
+from .process import process_page
 from .storage import list_revisions, read_file
+from .domain.fixes import WikitextFixerService
 
 __all__ = [
+    "WikitextFixerService",
     "remove_data_parsoid",
     "process_page",
-    "WikitextFixerService",
     "list_revisions",
     "read_file",
 ]
