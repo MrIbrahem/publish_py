7b8b5e409b2dcf1af23751f32787aea4b8a60186
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-20 00:39:39 +0300

    Update translate_med.py

--- src/main_app/public/routes/td/translate_med.py
+++ src/main_app/public/routes/td/translate_med.py
@@ -8,8 +8,11 @@ import logging
 
 from flask import (
     Blueprint,
+    redirect,
     request,
 )
+
+from ....services.utils.wiki_links import content_translation_url, get_endpoint
 
 from ....database.services import InProcessService
 from ....services.auth import get_current_user
@@ -31,14 +34,14 @@ class TranslateRoutes:
         for rule, method, target in routes:
             self.bp.route(rule, methods=[method])(target)
 
-    def index(self) -> str:
+    def index(self):
         """ """
         user = get_current_user()
         if user is None:
             return "Not logged in"
 
         langcode = request.args.get("langcode", type=str)
-        # camp = request.args.get("camp", type=str)
+        camp = request.args.get("camp", type=str, default="")
         word = request.args.get("word", type=int)
         cat = request.args.get("cat", type=str)
         tra_type = request.args.get("tra_type", type=str)
@@ -62,6 +65,13 @@ class TranslateRoutes:
                 word=word,
             )
 
+        return redirect(content_translation_url(
+            title=title,
+            code=langcode,
+            campaign=camp,
+            tra_type=tra_type,
+            endpoint=get_endpoint(),
+        ))
 
 __all__ = [
     "TranslateRoutes",
