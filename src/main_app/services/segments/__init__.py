055bf4bd00c2533c4fbb0e4af3013217e9f1754d
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-22 03:16:54 +0300

    Update __init__.py

--- src/main_app/services/segments/__init__.py
+++ src/main_app/services/segments/__init__.py
@@ -4,7 +4,7 @@ Library package for CX server.
 
 from __future__ import annotations
 
-from html_to_segments import process_html
+from .lib.processor import process_html
 
 __all__ = [
     "process_html",
